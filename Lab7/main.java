c11f1514d863b8fb11735427d6092abb1a11d1ee
mss_ktn <155761604+Krittanonn@users.noreply.github.com>
2025-01-07 15:08:01 +0700

    Update main.java

--- Lab7/main.java
+++ Lab7/main.java
@@ -1,1 +1,17 @@
+import Lib.*;
 
+public class main {
+    public static void main(String[] args) {
+        Chess chess = new Chess();
+        
+        System.out.println("Chess Properties:");
+        System.out.println("Category: " + chess.getCategory());
+        System.out.println("Minimum Age: " + chess.getMinAge());
+        System.out.println("Height: " + chess.getHeight());
+        System.out.println("Width: " + chess.getWidth());
+        System.out.println("Area: " + chess.calculateArea());
+        System.out.println("Circumference: " + chess.calculateCircumference());
+        
+        chess.play();
+    }
+}
